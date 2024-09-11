48dfe4bb16309918a1734947aca1499e23560813
shubham-oop <shubhams3232@gmail.com>
2024-09-11 07:19:20 +0530

    remove main branch un necessary code

--- src/main/java/TestConditon.java
+++ src/main/java/TestConditon.java
@@ -7,9 +7,6 @@ public class TestConditon {
         if (x > 15 || country.equals("US")) {
             System.out.println("Run Experiment");
         } else System.out.println("Not Run Experiment");
-        if(country.equals("US")) {
-            System.out.println("Main branch push");
-        }else System.out.println("Main branch pop");
         if(true) System.out.println("Feature Branch push");
     }
 }
